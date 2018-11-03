1d8da9ddc32109ea3001df3904dddbf7078b2d1f
vj2710 <v_ja@encs.concordia.ca>
2018-11-03 17:56:36 -0400

    Initial commit

---
+++ src/main/java/com/erp/demo/controller/HiringController.java
@@ -0,0 +1,26 @@
+package com.erp.demo.controller;
+
+import com.erp.demo.domain.HiringInfo;
+import com.erp.demo.service.HiringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hiring")
+public class HiringController {
+    @Autowired
+    private HiringService hiringService;
+
+    @PostMapping
+    public List<HiringInfo> getJoineeDetails(@RequestBody HiringInfo hiringInfo){
+        if(null == hiringInfo){
+            return null;
+        }
+        else{
+            hiringInfo.
+        }
+    }
+
+}
